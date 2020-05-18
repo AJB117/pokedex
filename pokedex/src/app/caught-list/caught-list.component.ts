bbd0674a30e64d872cbfc91feb176e101b82c333
Patrick Soga <psoga@nd.edu>
2020-05-17 20:26:25 -0700

    actually finished caught

--- pokedex/src/app/caught-list/caught-list.component.ts
+++ pokedex/src/app/caught-list/caught-list.component.ts
@@ -1,23 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Pokemon } from '../pokemon';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-caught-list',
   templateUrl: './caught-list.component.html',
-  styleUrls: ['./caught-list.component.css']
+  styleUrls: ['./caught-list.component.css'],
 })
 export class CaughtListComponent implements OnInit {
 
   caught: string[];
+  fillerText: string;
+  sub: any;
 
-  constructor() { }
+  remove(pkmn: string) {
+    let caught: any = JSON.parse(localStorage.getItem("caught"));
+    let name: string = pkmn['name'];
 
+    caught = caught.filter(c => {
+      if (c['name'] !== name) return c;
+    })
+    
+    localStorage.setItem("caught", JSON.stringify(caught))
+    this.router.navigate(['/caught']);
+  }
+
+  constructor(private router: Router, private route: ActivatedRoute) { 
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.sub = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.router.navigated = false;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
   ngOnInit(): void {
     this.caught = JSON.parse(localStorage.getItem("caught"));
-
-    console.log(this.caught)
-    // console.log(JSON.parse(this.caught[0])['types'][0]['type']['name'])
-    console.log(this.caught[1]['types'])
+    if (!this.caught.length) {
+      this.fillerText = "Add Pokemon you've caught";
+    }
   }
 
 }
