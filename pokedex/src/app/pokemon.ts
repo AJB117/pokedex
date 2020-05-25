09b88e211883f1c71d09d763b204c128fef0871e
Patrick Soga <psoga@nd.edu>
2020-05-24 17:53:24 -0700

    shortened using constructor

--- pokedex/src/app/pokemon.ts
+++ pokedex/src/app/pokemon.ts
@@ -1,7 +1,9 @@
 export class Pokemon {
-    name: string;
-    front_default: string;
-    type1: string;
-    type2: string;
-    number: number;
+    constructor(public name?: string, public front_default?: string, public type1?: string, public type2?: string, public number?: number) {
+        this.name = name;
+        this.front_default = front_default;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.number = number;
+    }
 }
